02015f099e537c650f909ae3cac302a82df4fb10
Luis Carlos Enríquez <luiscarlos_enriquez@me.com>
2021-12-28 19:20:16 -0600

    Reorder imports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/index';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
